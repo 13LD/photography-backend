7ca257a48458fa871e55cf252576875f6d92dc69
13LD <tompla96@ukr.net>
2018-04-03 00:00:04 +0300

    updated doc

--- app/controllers/api/concerns/swaggers/auth_swaggers.rb
+++ app/controllers/api/concerns/swaggers/auth_swaggers.rb
@@ -55,6 +55,50 @@ module Api::Concerns::Swaggers
           end
         end
       end
+      swagger_path '/api/logout' do
+
+        operation :post do
+          key :description, 'Destroy Token'
+          key :operationId, 'Destroy Token'
+          key :produces, [
+              'application/json'
+          ]
+          key :tags, [
+              'Auth'
+          ]
+          parameter do
+            key :name, 'email'
+            key :in, :query
+            key :description, 'Auth email'
+            key :required, false
+            key :type, :string
+            key :'$ref', :AuthInput
+          end
+
+
+          security do
+            key 'Authorization', []
+          end
+          response 200 do
+            key :description, 'Token destroyed successfully'
+            schema do
+              key :'$ref', :AuthInput
+            end
+          end
+          response 403 do
+            key :description, 'Permission denied'
+            schema do
+              key :'$ref', :AuthInput
+            end
+          end
+          response 422 do
+            key :description, 'Missing Parameters or Request rejected'
+            schema do
+              key :'$ref', :RejectRequest
+            end
+          end
+        end
+      end
     end
   end
 end
